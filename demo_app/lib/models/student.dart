c4f740e68c2c6301acd2b9bb9524ca17a7b9b4c0
jamal abdirahim <jamaalabdirahim@gmail.com>
2026-04-18 21:22:25 +0300

    Remove accidental folder

--- demo_app/lib/models/student.dart
+++
@@ -1,8 +0,0 @@
-class Student {
-  String name;
-  double percentage;
-  double gpa;
-  String grade;
-
-  Student(this.name, this.percentage, this.gpa, this.grade);
-}
--- demo_app/lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const gradient = LinearGradient(
-    colors: [Color(0xFF6A11CB), Color(0xFF2575FC)],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-  );
-}
